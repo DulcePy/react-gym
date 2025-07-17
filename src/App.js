69a53a5ab7cb6ab4da52c40f33124f91d687aef4
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2025-07-17 19:18:21 -0300

    Created the basic structure of the project

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App min-h-screen w-full bg-black relative">
+    {/* Midnight Mist */}
+    <div
+      className="absolute inset-0 z-0"
+      style={{
+        backgroundImage: `
+          radial-gradient(circle at 50% 100%, rgba(70, 85, 110, 0.5) 0%, transparent 60%),
+          radial-gradient(circle at 50% 100%, rgba(99, 102, 241, 0.4) 0%, transparent 70%),
+          radial-gradient(circle at 50% 100%, rgba(181, 184, 208, 0.3) 0%, transparent 80%)
+        `,
+      }}
+    />
+    {/* Your Content/Components */}
+
+      <h1 className="text-3xl font-bold underline text-sky-600"> Hello world! </h1>
+    
+  </div>
   );
 }
+
+
 
 export default App;
